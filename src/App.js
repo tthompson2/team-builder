baa85856c68e3482a20dcb3a5b5a7e27112c1482
Trevor Thompson <trevthom18@gmail.com>
2019-11-06 21:53:48 -0500

    Successfully managed to create a form for each of the different elements that they want us to track

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState({name: "", email: "", role: " "});
+
+  const changeHandler = event => {
+    setValue({...value, [event.target.name]: event.target.value});
+  }
+
+  const handleSubmission = event => {
+    event.preventDefault();
+    console.log(value.name);
+    console.log(value.email);
+    console.log(value.role);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(value)};
+          <form onSubmit={event => handleSubmission(event)}>
+              <label>
+                Name:
+                <input
+                    type="text"
+                    name="name"
+                    value={value.name}
+                    onChange={event => changeHandler(event)}
+                />
+              </label>
+              <label>
+                <input
+                    type="text"
+                    name="email"
+                    value={value.email}
+                    onChange={event => changeHandler(event)}
+                />
+              </label>
+              <label>
+                <input
+                   type="text"
+                   name="role"
+                   value={value.role}
+                   onChange={event => changeHandler(event)}
+                />
+              </label>
+         </form>
     </div>
   );
 }
