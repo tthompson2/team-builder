3a425a2ee115dfce60c140274b20558bf3558301
Trevor Thompson <trevthom18@gmail.com>
2020-01-21 20:54:20 -0500

    Created the Form.js file as required by the MVP of this assignment, begun working on the stretch assignment

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import Form from './Form';
 import './App.css';
 
 function App() {
-  const [value, setValue] = useState({name: "", email: "", role: " "});
 
-  const changeHandler = event => {
-    setValue({...value, [event.target.name]: event.target.value});
-  }
-
-  const handleSubmission = event => {
-    event.preventDefault();
-    console.log(value.name);
-    console.log(value.email);
-    console.log(value.role);
+  const memberToEdit = props => {
+     console.log(props);
+     console.log("test");
   }
 
   return (
-    <div className="App">
-      {console.log(value)};
-          <form onSubmit={event => handleSubmission(event)}>
-              <label>
-                Name:
-                <input
-                    type="text"
-                    name="name"
-                    value={value.name}
-                    onChange={event => changeHandler(event)}
-                />
-              </label>
-              <label>
-                <input
-                    type="text"
-                    name="email"
-                    value={value.email}
-                    onChange={event => changeHandler(event)}
-                />
-              </label>
-              <label>
-                <input
-                   type="text"
-                   name="role"
-                   value={value.role}
-                   onChange={event => changeHandler(event)}
-                />
-              </label>
-         </form>
-    </div>
+      <div>
+        <Form memberToEdit={memberToEdit}/>
+      </div>
   );
 }
 
